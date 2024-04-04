720490fd21cbeeab68bc3fe63fad0f0879bb7a60
Alessandro Muscio <alessandro.muscio@REGESTA.LOCAL>
2024-04-04 17:50:08 +0200

    feat: working on PrettyStrings class start

---
+++ it/kibo/fp/lib/PrettyStrings.py
@@ -0,0 +1,18 @@
+class PrettyStrings:
+    """
+    This class contains various methods to better print strings to the terminal.
+    """
+
+    SAPCE = " "
+    HORIZONTAL_FRAME = "-"
+    VERTICAL_FRAME = "|"
+    NEW_LINE = "\n"
+
+    def __init__(self) -> None:
+        """Prevents instantiation of this class
+
+        Raises:
+            NotImplementedError
+        """
+
+        raise NotImplementedError()
