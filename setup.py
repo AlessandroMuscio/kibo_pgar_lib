56b890eb8f44e86044e9247a256939376329c825
Alessandro Muscio <alessandro.muscio@REGESTA.LOCAL>
2024-04-05 11:41:52 +0200

    build: added settings file for publishing

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name="kibo_unibs_fp_lib",
     version="1.0.0",
     packages=find_packages(),
-    description="Libreria UniBSFpLib modificata da me",
-    author="Kibo",
+    description="Libreria UniBSFpLib modificata e convertita in Python da me",
+    author="Alessandro Muscio",
     author_email="a.muscio001@unibs.studenti.it",
     url="https://github.com/AlessandroMuscio/kibo_unibs_fp_lib",
 )
