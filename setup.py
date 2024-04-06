6761c0fd8f05b70fcc5fc93ec8e7c7e80b4d286e
Alessandro Muscio <alessandro.muscio@REGESTA.LOCAL>
2024-04-06 17:29:51 +0200

    build: changed build setup, sources weren't being shipped

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ from setuptools import setup, find_packages
 setup(
     name="kibo_unibs_fp_lib",
     version="1.0.0",
-    packages=find_packages(),
+    packages=["it.kibo.fp.lib"],
     description="UniBSFpLib modified, documented and converted in python.",
     author="Alessandro Muscio",
     author_email="a.muscio001@unibs.studenti.it",
-    url="https://github.com/AlessandroMuscio/kibo_unibs_fp_lib"
+    url="https://github.com/AlessandroMuscio/kibo_unibs_fp_lib",
 )
