709b22dd72506cd1fb2d12e7f7fb2a95d9bd2756
AlessandroMuscio <muscioalex30@gmail.com>
2024-04-06 17:17:08 +0200

    build: changed build files

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
     name="kibo_unibs_fp_lib",
     version="1.0.0",
     packages=find_packages(),
-    description="Libreria UniBSFpLib modificata e convertita in Python da me",
+    description="UniBSFpLib modified, documented and converted in python.",
     author="Alessandro Muscio",
     author_email="a.muscio001@unibs.studenti.it",
-    url="https://github.com/AlessandroMuscio/kibo_unibs_fp_lib",
+    url="https://github.com/AlessandroMuscio/kibo_unibs_fp_lib"
 )
