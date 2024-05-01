1f481034934c3974fd4957a09305b9535e9b3db6
AlessandroMuscio <muscioalex30@gmail.com>
2024-05-01 22:07:02 +0200

    feat: esier to import the library now

---
+++ kibo_unibs_fp_lib/__init__.py
@@ -0,0 +1,7 @@
+from .ansi_colors import AnsiColors
+from .file_service import FileService
+from .input_data import InputData
+from .known_problems import KnownProblems
+from .menu import Menu
+from .pretty_strings import PrettyStrings
+from .random_draws import RandomDraws
