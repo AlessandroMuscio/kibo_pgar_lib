c26d8c8a9484e205de6e21ba952288887171748b
AlessandroMuscio <muscioalex30@gmail.com>
2024-05-02 14:24:56 +0200

    refactor: better import support

--- kibo_unibs_fp_lib/__init__.py
+++ kibo_unibs_fp_lib/__init__.py
@@ -1,9 +1,9 @@
 """Module for setup configuration of kibo_unibs_fp_ib"""
 
-from kibo_unibs_fp_lib.ansi_colors import AnsiColors
-from kibo_unibs_fp_lib.file_service import FileService
-from kibo_unibs_fp_lib.input_data import InputData
-from kibo_unibs_fp_lib.known_problems import KnownProblems
+from kibo_unibs_fp_lib.ansi_colors import *
+from kibo_unibs_fp_lib.file_service import *
+from kibo_unibs_fp_lib.input_data import *
+from kibo_unibs_fp_lib.known_problems import *
 from kibo_unibs_fp_lib.menu import Menu
-from kibo_unibs_fp_lib.pretty_strings import PrettyStrings
-from kibo_unibs_fp_lib.random_draws import RandomDraws
+from kibo_unibs_fp_lib.pretty_strings import *
+from kibo_unibs_fp_lib.random_draws import *
