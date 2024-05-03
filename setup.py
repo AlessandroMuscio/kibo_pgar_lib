006ce7f6347c966f1b9207a0586e0ee3511dc337
AlessandroMuscio <muscioalex30@gmail.com>
2024-05-03 09:37:40 +0200

    refactor: changed library name

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="kibo_unibs_fp_lib",
+    name="kibo_pgar_lib",
     version="1.1.0",
     packages=find_packages(),
     description="KiboUniBSFpLib modified, documented and converted in python.",
     author="Alessandro Muscio",
     author_email="a.muscio001@unibs.studenti.it",
-    url="https://github.com/AlessandroMuscio/kibo_unibs_fp_lib",
+    url="https://github.com/AlessandroMuscio/kibo_pgar_lib",
 )
